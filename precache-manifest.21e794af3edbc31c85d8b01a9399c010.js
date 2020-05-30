6aa23d5f29265cdbe734c16b572d058f2b09ba24
DanielCortild <daniel@cortild.dk>
2020-05-30 04:50:56 +0200

    Updates

---
+++ precache-manifest.21e794af3edbc31c85d8b01a9399c010.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01fdca16268b69b0803d8b7fb0d6cedb",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "4da77f9c2e23fb96b346",
+    "url": "/react-portfolio/static/css/main.ef4c802e.chunk.css"
+  },
+  {
+    "revision": "d110b4fab1842717fedf",
+    "url": "/react-portfolio/static/js/2.4f2a4dbb.chunk.js"
+  },
+  {
+    "revision": "40be5ea31f0ae9d6ed3e05bd4f0b277c",
+    "url": "/react-portfolio/static/js/2.4f2a4dbb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4da77f9c2e23fb96b346",
+    "url": "/react-portfolio/static/js/main.5fc4b563.chunk.js"
+  },
+  {
+    "revision": "be3cb1a3dfa81366eb8c",
+    "url": "/react-portfolio/static/js/runtime-main.5ca49836.js"
+  }
+]);
