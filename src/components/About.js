71c6fbc8de73453b0d7e3ba63cf284c1de0b54d6
molk95 <molk.saouabi@gmail.com>
2020-09-09 14:53:35 +0200

    customize

--- src/components/About.js
+++ src/components/About.js
@@ -11,19 +11,19 @@ const About = () => {
   <section style={styles.about} className="flex-column">
 			<div style={styles.profileImg}></div>
 			<div style={styles.details}>
-				<h2 style={styles.title} className="font-weight-bold">Hi, I'm Daniel.<br />
+				<h2 style={styles.title} className="font-weight-bold">Hi, I'm Molk.<br />
         <Typed className="font-weight-light"
           strings={['I solve problems', 'I solve complex problems', 'I solve your problems.']}
           backSpeed={70} typeSpeed={60} backDelay={1000}
           /></h2>
 			</div>
-			<div><a className="my-button" href="mailto:daniel.cortild@gmail.com">Get In Touch</a></div>
+			<div><a className="my-button" href="mailto:molk.saouabi@gmail.com">Get In Touch</a></div>
 
 			<div className="text-center mt-4">
-				<a href="https://github.com/DanielCortild">
+				<a href="https://github.com/molk95">
           <FaGithub size="28"/>
         </a>
-				<a href="https://www.linkedin.com/in/daniel-cortild-67413a166/">
+				<a href="https://www.linkedin.com/in/molk-saouabi">
           <FaLinkedin size="28"/>
         </a>
 			</div>
@@ -42,7 +42,7 @@ var styles = {
     textAlign: 'center'
   },
   profileImg: {
-    backgroundImage: `url(${process.env.PUBLIC_URL}/images/profile.jpg)`,
+    backgroundImage: `url(${process.env.PUBLIC_URL}/images/profile.jpeg)`,
     width: '200px',
     height: '200px',
     margin: '0 auto',
--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,48 +1,45 @@
-import React from 'react';
-import {Row, Col} from 'reactstrap';
+import React from "react";
+import { Row, Col } from "reactstrap";
 
-import {RiComputerLine} from 'react-icons/ri';
-import {AiOutlineCloudServer, AiFillTool} from 'react-icons/ai';
+import { RiComputerLine } from "react-icons/ri";
+import { AiOutlineCloudServer, AiFillTool } from "react-icons/ai";
 
-const ExpTitle = ({title}) => {
-  return (
-    <h3 className="font-weight-bold text-white">{title}</h3>
-  )
-}
+const ExpTitle = ({ title }) => {
+  return <h3 className="font-weight-bold text-white">{title}</h3>;
+};
 
 const Experience = () => {
   return (
-    <Row className="font-weight-bold py-5 px-3" style={{backgroundColor: '#4c6d86', color: '#BBD2FF'}}>
+    <Row
+      className="font-weight-bold py-5 px-3"
+      style={{ backgroundColor: "#4c6d86", color: "#BBD2FF" }}
+    >
       <Col className="text-center">
-        <RiComputerLine size="4em" color="#FF8A59"/>
+        <RiComputerLine size="4em" color="#FF8A59" />
         <ExpTitle title="Front-end" />
         <p className="m-0">HTML5</p>
         <p className="m-0">CSS3</p>
-        <p className="m-0">Bootstrap</p>
         <p className="m-0">JavaScript</p>
-        <p className="m-0">JQuery</p>
+        <p className="m-0">Typescript</p>
         <p className="m-0">ReactJS</p>
       </Col>
       <Col className="text-center">
-        <AiOutlineCloudServer size="4em" color="#FF8A59"/>
+        <AiOutlineCloudServer size="4em" color="#FF8A59" />
         <ExpTitle title="Back-end" />
         <p className="m-0">NodeJS</p>
         <p className="m-0">NPM</p>
         <p className="m-0">ExpressJS</p>
-        <p className="m-0">MySQL</p>
-        <p className="m-0">Ruby on Rails</p>
-        <p className="m-0">R Programming</p>
+        <p className="m-0">MangoDB</p>
       </Col>
       <Col className="text-center">
-        <AiFillTool size="4em" color="#FF8A59"/>
+        <AiFillTool size="4em" color="#FF8A59" />
         <ExpTitle title="Tools" />
         <p className="m-0">VSCode</p>
-        <p className="m-0">Atom</p>
+        <p className="m-0">Postman</p>
         <p className="m-0">Github</p>
-        <p className="m-0">LaTeX</p>
       </Col>
     </Row>
-  )
-}
+  );
+};
 
 export default Experience;
